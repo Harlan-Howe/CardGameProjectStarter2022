b3b4f27a9c2e4a626366e5c3aaea9e497a8cbc81
Harlan Howe <hhowe7@gatech.edu>
2022-09-22 12:36:57 -0500

    Updated documentation with change of language Stack -> Pile and intro to Card class.

--- docs/type-search-index.js
+++ docs/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Card"},{"p":"<Unnamed>","l":"CardPile"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Card"},{"p":"<Unnamed>","l":"CardPile"}];updateSearchResults();
